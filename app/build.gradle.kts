a504c82d35ef775d87099e78e6fbb6196c749443
Matias Sanchez <matias.fss97@gmail.com>
2024-03-26 16:22:37 -0300

    Quinto Commit "Pruebas actualizadas con zxing para leer codigo QR sin funciones deprecadas"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,5 +76,5 @@ dependencies {
 
     //zxing
     implementation("com.google.zxing:core:3.3.0")
-    implementation("com.journeyapps:zxing-android-embedded:4.1.0")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 }
--- app/src/main/java/com/copetiny/proyecto/ui/scan/ScanFragment.kt
+++ app/src/main/java/com/copetiny/proyecto/ui/scan/ScanFragment.kt
@@ -2,14 +2,18 @@ package com.copetiny.proyecto.ui.scan
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import com.copetiny.proyecto.R
 import com.copetiny.proyecto.databinding.FragmentScanBinding
 import com.google.zxing.integration.android.IntentIntegrator
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -27,23 +31,20 @@ class ScanFragment : Fragment() {
         binding.btnScan.setOnClickListener {initBtnScan()}
     }
     private fun initBtnScan(){
-        val integrator = IntentIntegrator(this.requireActivity())
-        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
-        integrator.setPrompt("Escanea el Material que quieres reciclar")
-        integrator.setBeepEnabled(true)
-        integrator.initiateScan()
+        val scanOptions = ScanOptions()
+        scanOptions.setPrompt("Escanea el material que quieres reciclar")
+        scanOptions.setBeepEnabled(true)
+        scanOptions.setOrientationLocked(true)
+        scanOptions.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+        barcodeLauncher.launch(scanOptions)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
-        if(result != null){
-            if(result.contents == null){
-                Toast.makeText(this.requireContext(), "Cancelado", Toast.LENGTH_SHORT).show()
-            }else{
-                Toast.makeText(this.requireContext(), "el valor escaneado es: ${result.contents}", Toast.LENGTH_SHORT).show()
-            }
+    private val barcodeLauncher = registerForActivityResult(ScanContract()){ result ->
+        if(result.contents == null){
+            Toast.makeText(requireContext(), "Error al Leer el codigo QR", Toast.LENGTH_SHORT).show()
         }else{
-            super.onActivityResult(requestCode, resultCode, data)
+            Toast.makeText(requireContext(), "el valor escaneado es: ${result.contents}", Toast.LENGTH_SHORT).show()
+            Log.i("Matias","${result.contents}")
         }
     }
 
